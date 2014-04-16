3d97dbdf82bed1a2ac156f4439ad94df9cb426cb
BrwnRyce <brwnryce@gmail.com>
2014-04-16 02:18:33 -0500

    added sound class, should work with very simple method call, will test further when sound files are acquired

---
+++ angraves-zombie-game/src/Sound.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import javax.sound.sampled.*;
+
+public class Sound {
+	
+	public static synchronized void play(final String fileName){ //works for "Sound.play("FILE_NAME.wav");"
+		
+		new Thread(new Runnable() {
+			
+			public void run() {
+				try{
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(fileName)); 	//.wav files
+					clip.open(inputStream);																	//full file path required? or maybe just local?
+					clip.start();
+				}catch (Exception e) {
+					System.out.println("error playing sound: " + e.getMessage()+ " for " + fileName);
+				}
+			}
+
+		}).start(); //end of thread
+	}
+}
